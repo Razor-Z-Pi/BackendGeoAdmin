1ff47cbf6f6263dbe07ec73d4a973a8511021431
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-08-09 00:17:17 +0700

    request-controller

--- app/Http/Controllers/User/StoreController.php
+++ app/Http/Controllers/User/StoreController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\StoreRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class StoreController extends Controller
 {
-    public function __invoke(): int
+    public function __invoke(StoreRequest $request)
     {
-      return 1111;
+      $data = $request -> validated();
+      $data["password"] = Hash::make($data["password"]);
+      User::firstOrCreate([
+        "email" => $data["email"],
+      ], $data);
+      return response([]);
     }
 }
